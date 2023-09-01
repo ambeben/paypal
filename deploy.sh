8c433cad5a43a48248b108ae35b42412e97fb0b2
Ben Ambe <ambeben@gmail.com>
2023-09-01 14:07:28 -0500

    done

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
-deployment is ready
+deployment is reay
 in uat
 qu testing completed
 performance testing is good
+<<<<<<< HEAD
 qa testing completed
 performance testing is good
+=======
+
+>>>>>>> f4b8e96 (rich indeed)
